b18a8485b91ee03b86bc3a8e9edf4eefc5430113
Kayce Basques <kaycebasques@gmail.com>
2026-06-04 21:17:17 -0700

    Start Sphinx theme

---
+++ sphinx/theme/bazel/docs/conf.py
@@ -0,0 +1,8 @@
+# Project
+project = "theme"
+release = "0.0.0"
+author = "Kayce Basques"
+copyright = f"2026, {author}"
+
+# HTML
+html_theme = 'basic'
---
+++ sphinx/theme/bazel/docs/index.rst
@@ -0,0 +1,5 @@
+=====
+theme
+=====
+
+Hello, world!
