7163dd50f85db2e26d5d5b6da994c4a4b55eccd0
Kayce Basques <kaycebasques@gmail.com>
2026-06-06 03:55:24 +0000

    Set up tests

---
+++ sphinx/theme/bazel/tests/test_html.py
@@ -0,0 +1,24 @@
+import unittest
+from harness import SphinxTestBase
+
+
+class TestHtml(SphinxTestBase):
+    def test_title(self):
+        outdir = self.build_docs(
+            conf_content="""
+                project = "test_html"
+                extensions = ["theme"]
+                html_theme = "theme"
+            """,
+            index_content="""
+                ==========
+                test_title
+                ==========
+            """
+        )
+        html = (outdir / "index.html").read_text()
+        self.assertIn("<title>test_title</title>", html)
+
+
+if __name__ == "__main__":
+    unittest.main()
